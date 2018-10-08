7778e702d7444faa064a46c46c92fa23edfcdca4
Jose Castillo <jose.castillo@claranet.uk>
2018-10-08 12:53:50 +0100

    Add vpc cidr block output

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,6 +3,11 @@ output "vpc_id" {
   value       = "${aws_vpc.vpc.id}"
 }
 
+output "vpc_cidr" {
+  description = "The CIDR block of the VPC"
+  value       = "${aws_vpc.vpc.cidr_block}"
+}
+
 output "internet_gateway_id" {
   description = "The Internet Gateway ID"
   value       = "${aws_internet_gateway.igw.id}"
