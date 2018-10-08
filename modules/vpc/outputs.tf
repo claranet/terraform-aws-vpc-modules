336bf294fa6ba10f31c4549c14b2f9083b55f4db
Jose Castillo <jose.castillo@claranet.uk>
2018-10-08 13:04:38 +0100

    Rename vpc cidr block output resource

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,7 +3,7 @@ output "vpc_id" {
   value       = "${aws_vpc.vpc.id}"
 }
 
-output "vpc_cidr" {
+output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
   value       = "${aws_vpc.vpc.cidr_block}"
 }
